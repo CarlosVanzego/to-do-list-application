d8815eb40385935d17fa360aded47603d577ba7c
Your Name <carlosvanzego96@gmail.com>
2023-12-08 21:29:58 -0500

    Finished To-do-list-application

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,10 @@ type Task = {
 console.log(uuidV4())
 
 const list = document.querySelector<HTMLUListElement>("#list")
-const form = document.getElementById("#new-task-form") as HTMLFormElement | null
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
 const tasks: Task[] = loadTasks()
 tasks.forEach(addListItem)
-
 form?.addEventListener("submit", e => {
   e.preventDefault()
 
